43018cfbc06e6fbbdecf5c855e5d6b6093389e06
jhowell1 <jhowell1@mail.csuchico.edu>
2020-09-09 11:59:03 -0700

    Added multiplication to main.cpp

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main()
+{
+    cout<<"Hi, please enter two numbers: ";
+
+    int x,y;
+
+    cin >> x >> y;
+    cout << x + y << endl;
+
+    return 0;
+}
+
